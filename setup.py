465ca646e050d5f2bdee07fc07f77576243d7147
Guillaume Ludinard <guillaume.ludi@gmail.com>
2021-08-19 14:44:08 +0900

    External call support
    Some plugins (like google chat plugin) would like to present the CLI
    and its options to the user, but we would like to avoid having to rely
    on subprocess calls.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="snooze-client",
-    version='1.0.8',
+    version='1.0.9',
     author='Guillaume Ludinard, Florian Dematraz',
     author_email='guillaume.ludi@gmail.com, ',
     description="Client library for snooze server",
