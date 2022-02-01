b923e51ae0832f3e6d871fb690b70149f3d598aa
Guillaume Ludinard <guillaume.ludi@gmail.com>
2022-02-01 11:25:38 +0900

    pass_server was missing for alert and login

--- snooze_client/cli/alert.py
+++ snooze_client/cli/alert.py
@@ -2,10 +2,11 @@
 
 import click
 
-from snooze_client.cli.utils import parse_kv
+from snooze_client.cli.utils import parse_kv, pass_server
 
 @click.command()
 @click.argument('keyvalues', nargs=-1)
+@pass_server
 def alert(server, keyvalues):
     '''Raise an alert in snooze'''
     record = parse_kv(keyvalues)
--- snooze_client/cli/login.py
+++ snooze_client/cli/login.py
@@ -3,8 +3,10 @@
 import click
 
 from snooze_client.cli import snoozegroup
+from snooze_client.cli.utils import pass_server
 
 @snoozegroup.command()
 @click.option('--auth-method', '-A', type=click.Choice(['local', 'ldap', 'jwt']), help='Authentication method to use.')
+@pass_server
 def login(server):
     pass
